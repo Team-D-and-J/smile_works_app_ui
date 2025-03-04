9b89fb7393919752c344e2320e3e00f94ffc0280
Thomas Gooch <teg824@hotmail.com>
2025-03-04 15:56:48 -0600

    initial view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex h-20  justify-between">
+		<div className="flex flex-col h-20 w-full  justify-between">
 			<div className="flex mt-2 p-4  h-20 items-center ">Home</div>
-			<div className="flex mt-2 p-4  h-20 items-center ">
+			<div className="flex flex-col mt-2 p-4  h-20 items-center ">
+				<p className="text-2xl">Welcome user.name!</p>
 				<button className="p-2">SignIn</button>
 			</div>
 		</div>
