e0e8c63c0fc2ecb2f5eee7fcda2f7aa3d6b3ef31
timo9036 <timo9036@hotmail.com>
2025-03-09 20:35:13 -0500

    first attempt to convert vanillajs to reactjs

--- src/app/patientsearch/page.tsx
+++ src/app/patientsearch/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
-
 const page = () => {
-	return <div>page</div>;
-};
-
+  return <>
+  <div className="treatment-grid">
+          <button>Dental Restoration</button>
+          <button>Invisalign</button>
+          <button id="tooth-extractions">Tooth Extractions</button>
+          <button>Complete Exams</button>
+          <button>Veneer</button>
+          <button>Crown</button>
+          <button>Teeth Whitening</button>
+          <button>Mouthguard</button>
+          <button>Dental Bonding</button>
+          <button>Bridge</button>
+          <button>placeholder1</button>
+          <button>placeholder2</button>
+        </div>
+        
+        <div id="pdf-modal" className="modal">
+          <div className="modal-content">
+            <span className="close">&times;</span>
+            <iframe id="pdf-frame" src="" frameborder="0"></iframe>
+          </div>
+        </div>
+	
+        </>
 export default page;
