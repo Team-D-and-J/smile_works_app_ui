639e58b0a45297a91af169f12e958849a444ce4b
timo9036 <timo9036@hotmail.com>
2025-03-10 00:50:03 -0500

    revert changes in patient search

--- src/app/patientsearch/page.tsx
+++ src/app/patientsearch/page.tsx
@@ -1,26 +1,7 @@
+import React from "react";
+
 const page = () => {
-  return <>
-  <div className="treatment-grid">
-          <button>Dental Restoration</button>
-          <button>Invisalign</button>
-          <button id="tooth-extractions">Tooth Extractions</button>
-          <button>Complete Exams</button>
-          <button>Veneer</button>
-          <button>Crown</button>
-          <button>Teeth Whitening</button>
-          <button>Mouthguard</button>
-          <button>Dental Bonding</button>
-          <button>Bridge</button>
-          <button>placeholder1</button>
-          <button>placeholder2</button>
-        </div>
-        
-        <div id="pdf-modal" className="modal">
-          <div className="modal-content">
-            <span className="close">&times;</span>
-            <iframe id="pdf-frame" src="" frameborder="0"></iframe>
-          </div>
-        </div>
-	
-        </>
+  return <div>page</div>;
+};
+
 export default page;
