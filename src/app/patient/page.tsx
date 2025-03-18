2da5c831ebcad6b040819e9f60d19805ed32a689
Jake Nead <jakenead@gmail.com>
2025-03-17 18:42:27 -0700

    Connect to backend and modify styles

--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SearchResult from "@/components/patient/SearchResult";
 import Link from "next/link";
-import data from "./mockData";
 
 export interface Patient {
   _id: string;
@@ -23,15 +22,16 @@ interface Address {
   zip: string;
 }
 
-const RESULTS_PER_PAGE = 20;
+const RESULTS_PER_PAGE = 15;
 
 const Page = () => {
-  const [results, setResults] = useState<Patient[]>(data);
+  const [results, setResults] = useState<Patient[]>([]);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [dob, setDob] = useState("");
-  const [patientId, setPatientId] = useState("");
+  const [_id, set_id] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const totalResults = results.length;
   const totalPages = Math.ceil(totalResults / RESULTS_PER_PAGE);
@@ -39,15 +39,11 @@ const Page = () => {
   const endIndex = Math.min(startIndex + RESULTS_PER_PAGE, totalResults);
   const currentResults = results.slice(startIndex, endIndex);
 
-  useEffect(() => {
-    // fetch filtered patients here
-  }, []);
-
   function handleClearFilters() {
     setFirstName("");
     setLastName("");
     setDob("");
-    setPatientId("");
+    set_id("");
   }
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -59,10 +55,11 @@ const Page = () => {
         {
           method: "GET",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ name, dob, patientId }),
+          body: JSON.stringify({ name, dob, _id }),
         }
       );
       if (response.ok) {
+        setErrorMessage("");
         const data = await response.json();
         data.sort((a: Patient, b: Patient) => {
           return b._metadata.lastUpdatedAt - a._metadata.lastUpdatedAt;
@@ -72,18 +69,19 @@ const Page = () => {
       }
     } catch (error) {
       if (error instanceof Error) {
+        setErrorMessage(error.message);
         console.error(error.message);
       }
     }
   }
   return (
     <div className="w-full p-8">
-      <div className="max-w-7xl ">
-        <div className="flex justify-between items-center mb-10 max-w-7xl">
+      <div className="max-w-7xl flex flex-col items-center ">
+        <div className="flex justify-between items-center mb-6 w-full max-w-7xl">
           <h1 className="text-2xl font-bold">Patient Search</h1>
           <div>
             <Link
-              href="/patienteducation"
+              href="/patientEducation"
               className="p-2 mr-4 border border-gray-300 rounded-md"
             >
               Patient Education
@@ -96,81 +94,88 @@ const Page = () => {
             </Link>
           </div>
         </div>
-        <form
-          onSubmit={handleSubmit}
-          className="flex items-end gap-1 justify-between my-5 max-w-7xl"
-        >
-          <div className="flex flex-col">
-            <label htmlFor="firstName" className="text-sm font-medium">
-              First Name
-            </label>
-            <input
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              type="text"
-              id="firstName"
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="lastName" className="text-sm font-medium">
-              Last Name
-            </label>
-            <input
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              type="text"
-              id="lastName"
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="patientId" className="text-sm font-medium">
-              Patient ID
-            </label>
-            <input
-              value={patientId}
-              onChange={(e) => setPatientId(e.target.value)}
-              type="text"
-              id="patientId"
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="dob" className="text-sm font-medium">
-              Date of Birth
-            </label>
-            <input
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-              type="date"
-              id="dob"
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <button
-            type="submit"
-            className="flex justify-center items-center p-2 h-12 w-24 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-            title="search"
+        <div className="flex flex-col justify-center max-w-full">
+          <form
+            onSubmit={handleSubmit}
+            className="flex items-end gap-2 justify-between mt-4 w-full max-w-5xl"
           >
-            Search
-            {/* <FaSearch /> */}
-          </button>
-        </form>
-        <p
-          onClick={handleClearFilters}
-          className="place-self-end font-medium cursor-pointer mr-1 mb-4"
-        >
-          Clear filters
-        </p>
-        <div className="w-full flex flex-col gap-2">
+            <div className="flex flex-col">
+              <label htmlFor="firstName" className="text-sm font-medium">
+                First Name
+              </label>
+              <input
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                type="text"
+                id="firstName"
+                className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="lastName" className="text-sm font-medium">
+                Last Name
+              </label>
+              <input
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                type="text"
+                id="lastName"
+                className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="_id" className="text-sm font-medium">
+                Patient ID
+              </label>
+              <input
+                value={_id}
+                onChange={(e) => set_id(e.target.value)}
+                type="text"
+                id="_id"
+                className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="dob" className="text-sm font-medium">
+                Date of Birth
+              </label>
+              <input
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                type="date"
+                id="dob"
+                className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="flex justify-center items-center p-2 h-12 w-24 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+              title="search"
+            >
+              Search
+            </button>
+          </form>
+          <div className="flex justify-between items-center w-full my-4">
+            <p className="text-red-500 ">{errorMessage}</p>
+            <p
+              onClick={handleClearFilters}
+              className="font-medium cursor-pointer mr-1  "
+            >
+              Clear filters
+            </p>
+          </div>
+        </div>
+        <div className="w-full flex flex-col">
           {currentResults.map((patient) => (
             <SearchResult patient={patient} key={patient._id} />
           ))}
         </div>
-        <div className="flex justify-between items-center mt-6">
+        <div className="flex justify-between items-center mt-4 min-w-full">
           <span>
-            Showing {startIndex + 1}–{endIndex} of {totalResults}
+            {" "}
+            {results.length
+              ? `Showing ${startIndex + 1}–${endIndex} of ${totalResults}:`
+              : ""}
           </span>
           <div className="flex gap-2">
             {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
