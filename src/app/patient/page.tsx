d2a2d1895c67589306a8604fbef6491bff2aa1f3
Jake Nead <jakenead@gmail.com>
2025-03-19 16:54:11 -0700

    Add missing slash in the fetch url

--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -55,7 +55,7 @@ const Page = () => {
       if (dob) params.append("dob", dob);
       if (_id) params.append("_id", _id);
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}search?${params.toString()}`
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/search?${params.toString()}`
       );
       if (response.ok) {
         setErrorMessage("");
