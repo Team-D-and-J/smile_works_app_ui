44106f262311c7ecb688315b0751c45bb320a064
Thomas Gooch <teg824@hotmail.com>
2025-03-26 14:54:33 -0500

    admin update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const page = () => {
 	return (
-		<div className="flex flex-col ">
+		<div className="flex flex-col pt-[100px] justify-start items-center h-screen w-screen bg-[#F0F4F8]">
 			<h1 className="text-3xl p-4">Admin Page</h1>
 			<Link href="/admin/inventorysetup">
 				<div className="bg-[#0A3981] text-white text-center rounded-lg p-4 m-4">
@@ -13,6 +13,11 @@ const page = () => {
 			<Link href="/admin/clinic">
 				<div className="bg-[#0A3981] text-white text-center rounded-lg p-4 m-4">
 					<p className="text-xl">Clinic Setup</p>
+				</div>
+			</Link>
+			<Link href="/admin/users">
+				<div className="bg-[#0A3981] text-white text-center rounded-lg p-4 m-4">
+					<p className="text-xl">Users Setup</p>
 				</div>
 			</Link>
 		</div>
